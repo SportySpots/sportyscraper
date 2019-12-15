7724486710236115bd1887ab0bbe1cc25141f29c
ashutosh <ashutoshb@gmail.com>
2019-12-15 21:45:45 +0100

    restrict concurrent requests

--- sportyscraper/sportyscraper/settings.py
+++ sportyscraper/sportyscraper/settings.py
@@ -22,7 +22,7 @@ USER_AGENT = "Googlebot/2.1 (+http://www.googlebot.com/bot.html)"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
